24eb632c6eeab6a9c533774aa7d00a9208845620
eswarraogit <38001383+eswarraogit@users.noreply.github.com>
2018-04-24 17:56:11 +0530

    Update AppTest.java

--- src/test/java/com/github/jitpack/AppTest.java
+++ src/test/java/com/github/jitpack/AppTest.java
@@ -2,7 +2,7 @@ package com.github.jitpack;
 
 import org.junit.Test;   
 
-import static org.junit.Assert.assertEquals;  
+import static org.junit.Assert.assertEquals;       
 import static org.junit.Assert.assertTrue;
 
 public class AppTest 
